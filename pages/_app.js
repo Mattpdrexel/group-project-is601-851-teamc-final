7494a95d4dc07f81b75d6e610ba9d1d930720bee
Matthew Pennington <mattpdrexel@gmail.com>
2023-08-02 19:59:41 -0400

    Created buyer persona page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-// Import global CSS
-import '../styles/globals.css';
+import '../styles/globals.css'
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+function MyApp({ Component, PageProps }) {
+  return <Component {...PageProps} />
 }
+
+export default MyApp
