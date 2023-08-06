c23bdeeb67b27c0d7d7348d4dbdac8dc2641e58c
Matthew Pennington <mattpdrexel@gmail.com>
2023-08-06 04:40:26 -0400

    Testing issue with deploy

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -5,9 +5,19 @@ const PrivacyPolicy = () => {
 
   useEffect(() => {
     fetch('/privacypolicy.html')
-      .then(response => response.text())
+      .then(response => {
+        if (!response.ok) {
+          // If the response isn't ok, throw an error to be caught in the catch block
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
       .then(data => {
         setHtmlContent(data);
+      })
+      .catch(error => {
+        console.error('There was a problem fetching the privacy policy:', error.message);
+        setHtmlContent(`<p>Error loading content.</p>`);
       });
   }, []);
 
