f6fd5f7ca9ff7489f57800b06294ccd94b3c233a
PratikPathak07 <53492660+PratikPathak07@users.noreply.github.com>
2023-08-06 23:09:13 -0400

    index.js tests added

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,12 +6,28 @@ test('loads homepage and checks elements', async ({ page }) => {
   await page.goto(BASE_URL);
 
   // Check for the presence of the main heading
-  const heading = await page.textContent("h1");
-  expect(heading).toBe("Rise & Grind");
-  
+  const heading = await page.textContent('h1');
+  expect(heading).toBe('Rise & Grind');
+
   // Check for the presence of the logo image
   const logo = await page.$('img[alt="Rise & Grind Logo"]');
   expect(logo).toBeTruthy();
 
-  // Additional checks can be added...
+  // Check for the presence of the "Daily Delight" text
+  const dailyDelight = await page.textContent('h2');
+  expect(dailyDelight).toBe('Daily Delight');
+
+  // Check for the presence of the "Real Coffee, Real People" text
+  const realCoffeeText = await page.textContent('h1 > span');
+  expect(realCoffeeText).toBe('Coffee');
+  const realPeopleText = await page.textContent('h1 > span:last-child');
+  expect(realPeopleText).toBe('People');
+
+  // Check for the presence of the hero text
+  const heroText = await page.textContent('.heroText p');
+  expect(heroText).toContain("Rise with us. Grind with us. Start your day the 'Regular Joe' way. Experience high-quality, authentic coffee without the frills. Fuel your everyday pursuits with Rise & Grind.");
+
+  // Check for the presence of the newsletter form
+  const newsletterForm = await page.isVisible('form[name="newsletter"]');
+  expect(newsletterForm).toBeTruthy();
 });
