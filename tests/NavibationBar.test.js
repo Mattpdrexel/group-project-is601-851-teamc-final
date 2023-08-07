816a3d8b7487402ebcd6ffdb41ecf3181884f08a
PratikPathak07 <53492660+PratikPathak07@users.noreply.github.com>
2023-08-06 23:36:42 -0400

    add blog 2 tests

--- tests/NavibationBar.test.js
+++ tests/NavibationBar.test.js
@@ -10,36 +10,22 @@ test.describe('Navigation Bar Tests', () => {
   for (let pageUrl of pages) {
     test(`Navbar is visible on ${pageUrl}`, async ({ page }) => {
       await page.goto(`${BASE_URL}${pageUrl}`);
-
-      // Check for navigation bar based on its unique element, e.g., h1 text
       const navbarTitle = await page.$('h1');
       expect(await navbarTitle.innerText()).toBe('Rise & Grind');
-
-      // You can add more checks if needed, e.g., for individual links
     });
   }
 
   test('About Us page content', async ({ page }) => {
-    const pageUrl = '/aboutus'; // URL for the About Us page
+    const pageUrl = '/aboutus';
     await page.goto(`${BASE_URL}${pageUrl}`);
-
-    // Check for the presence of the navigation bar based on its unique element, e.g., h1 text
     const navbarTitle = await page.$('h1');
     expect(await navbarTitle.innerText()).toBe('Rise & Grind');
-
-    // Check for the presence of the "About Rise & Grind" heading
     const aboutUsHeading = await page.textContent('h1');
     expect(aboutUsHeading).toBe('About Rise & Grind');
-
-    // Check for the presence of the "Serving Everyday Heroes with Everyday Coffee" subtitle
     const subtitle = await page.textContent('h2');
     expect(subtitle).toBe('Serving Everyday Heroes with Everyday Coffee');
-
-    // Check for the presence of paragraphs on the page
     const paragraphs = await page.$$('p');
     expect(paragraphs.length).toBeGreaterThan(0);
-
-    // Add more specific checks for other elements on the About Us page if needed
   });
 
   test('Blog1 page renders correctly', async ({ page }) => {
@@ -56,6 +42,27 @@ test.describe('Navigation Bar Tests', () => {
     const image = await page.$('img[alt="Happy lady with coffee looking to side"]');
     expect(image).toBeTruthy();
     const nextButton = await page.$('a[href="/blog2"]');
+    expect(nextButton).toBeTruthy();
+  });
+
+  test('Blog2 page renders correctly', async ({ page }) => {
+    const pageUrl = '/blog2';
+    await page.goto(`${BASE_URL}${pageUrl}`);
+    const largeText = await page.textContent('.largeText');
+    expect(largeText).toContain(
+      '28-year-old Fiona chose Rise & Grind as her favorite gathering spot. Our casual atmosphere, consistent high-quality coffee, and no-fuss approach make it the perfect spot for her to connect with friends. In Rise & Grind, Fiona found a genuine, comfortable space to unwind and enjoy simple pleasures.'
+    );
+    const name = await page.textContent('.name');
+    expect(name).toBe('Fiona');
+    const mediumText = await page.textContent('.mediumText');
+    expect(mediumText).toBe('Loiterer');
+    const mainImage = await page.$('img[src="/images/Art/Unsplash/ana-itonishvili-Fyl8sMC2j2Q-unsplash.jpg"]');
+    expect(mainImage).toBeTruthy();
+    const leftQuote = await page.$('img[src="/images/Art/Quotes.png"]');
+    expect(leftQuote).toBeTruthy();
+    const rightQuote = await page.$('img[src="/images/Art/Quotes.png"]');
+    expect(rightQuote).toBeTruthy();
+    const nextButton = await page.$('a[href="/blog3"]');
     expect(nextButton).toBeTruthy();
   });
 
