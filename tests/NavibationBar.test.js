0045a0d4b418a02e8c4bc79c1d7f91b490783f41
PratikPathak07 <53492660+PratikPathak07@users.noreply.github.com>
2023-08-06 23:14:07 -0400

    add aboutus tests

--- tests/NavibationBar.test.js
+++ tests/NavibationBar.test.js
@@ -18,4 +18,28 @@ test.describe('Navigation Bar Tests', () => {
       // You can add more checks if needed, e.g., for individual links
     });
   }
+
+  test('About Us page content', async ({ page }) => {
+    const pageUrl = '/aboutus'; // URL for the About Us page
+    await page.goto(`${BASE_URL}${pageUrl}`);
+
+    // Check for the presence of the navigation bar based on its unique element, e.g., h1 text
+    const navbarTitle = await page.$('h1');
+    expect(await navbarTitle.innerText()).toBe('Rise & Grind');
+
+    // Check for the presence of the "About Rise & Grind" heading
+    const aboutUsHeading = await page.textContent('h1');
+    expect(aboutUsHeading).toBe('About Rise & Grind');
+
+    // Check for the presence of the "Serving Everyday Heroes with Everyday Coffee" subtitle
+    const subtitle = await page.textContent('h2');
+    expect(subtitle).toBe('Serving Everyday Heroes with Everyday Coffee');
+
+    // Check for the presence of paragraphs on the page
+    const paragraphs = await page.$$('p');
+    expect(paragraphs.length).toBeGreaterThan(0);
+
+    // Add more specific checks for other elements on the About Us page if needed
+  });
+
 });
